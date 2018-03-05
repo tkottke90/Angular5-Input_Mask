95cbf6b7924e2dcc11680a6213d9b6c544b05cea
tkottke90 <altotom90@gmail.com>
2018-03-04 22:17:26 -0600

    Building input logic for currency directive

--- src/app/currency.directive.ts
+++ src/app/currency.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCurrency]'
 })
 export class CurrencyDirective {
 
-  constructor() { }
+  basicNumber = new RegExp('[0-9]*(?!\.)');
+  advNumber = new RegExp('[0-9]*\.{1}[0-9]{2}');
+  maxNumber = new RegExp('[0-9]*\.{1}([0-9]{1})(?![0-9])');
+
+  prevVal = '';
+
+  @HostListener('input') onInput() {
+    console.log(this.el.nativeElement.value);
+
+    const userInput = this.el.nativeElement.value;
+
+    
+  }
+
+  constructor( private el: ElementRef ) { }
 
 }
+
+
+/*
+[0-9]*(?!\.)
+[0-9]*\.{1}[0-9]{1,2}
+
+*/
